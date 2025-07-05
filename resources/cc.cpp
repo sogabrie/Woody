5747238300655135fe2ebc1b40f75ad169ca6e9b
sogabrie <gabrielyan.sos@mail.ru>
2025-07-05 15:33:23 +0400

    update

--- resources/cc.cpp
+++ resources/cc.cpp
@@ -86,14 +86,12 @@ int main(int argc, char **argv) {
 
     const char *filename = argv[1];
 
-    // Открываем файл
     int fd = open(filename, O_RDONLY);
     if (fd < 0) {
         perror("open");
         return 1;
     }
 
-    // Получаем размер файла через fstat
     struct stat st;
     if (fstat(fd, &st) < 0) {
         perror("fstat");
@@ -101,7 +99,6 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // Мапим файл в память для удобства
     void *map = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (map == MAP_FAILED) {
         perror("mmap");
@@ -109,10 +106,8 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // ELF header
     Elf64_Ehdr *ehdr = (Elf64_Ehdr *) map;
 
-    // Проверяем ELF magic
     if (memcmp(ehdr->e_ident, ELFMAG, SELFMAG) != 0) {
         fprintf(stderr, "Not an ELF file.\n");
         munmap(map, st.st_size);
@@ -120,7 +115,6 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // Проверяем класс ELF
     if (ehdr->e_ident[EI_CLASS] != ELFCLASS64) {
         fprintf(stderr, "Not a 64-bit ELF file.\n");
         munmap(map, st.st_size);
@@ -128,28 +122,23 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // Переходим к таблице заголовков секций
     Elf64_Shdr *shdr_table = (Elf64_Shdr *) ((char *)map + ehdr->e_shoff);
     int shnum = ehdr->e_shnum;
 
-    // Индекс секции с именами секций
     Elf64_Shdr *sh_strtab = &shdr_table[ehdr->e_shstrndx];
     const char *const sh_strtab_p = (char *)map + sh_strtab->sh_offset;
 
-    // Ищем секцию с именем ".text"
     for (int i = 0; i < shnum; i++) {
         const char *name = sh_strtab_p + shdr_table[i].sh_name;
 
         if (strcmp(name, ".text") == 0) {
             printf("Found .text section!\n");
 
-            // Получаем offset и размер
             Elf64_Off offset = shdr_table[i].sh_offset;
             Elf64_Xword size = shdr_table[i].sh_size;
 
             printf(".text offset: 0x%lx, size: %lu bytes\n", offset, size);
 
-            // Читаем содержимое .text
             const unsigned char *text_data = (unsigned char *)map + offset;
 
             printf("First 16 bytes of .text:\n");
